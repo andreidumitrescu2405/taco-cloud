02fa394199f16676c501cf6b5c4eac99d64c0fe7
Andrei Dumitrescu <andreidumi2405@gmail.com>
2023-10-04 20:33:51 +0300

    methods implementation for querying h2 database with findAll and findById

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -1,9 +1,13 @@
 package tacos.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tacos.Ingredient;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,18 +15,31 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbcTemplate.query("select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient);
     }
 
     @Override
     public Optional<Ingredient> findById(String id) {
-        return Optional.empty();
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id);
+        return results.size() == 0 ? Optional.empty() : Optional.of(results.get(0));
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf(row.getString("type")));
     }
 
     @Override
